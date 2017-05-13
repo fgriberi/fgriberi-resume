8e3fb06ea22ff29920593a37257cc67a97ed8a62
Franco Riberi <fgriberi@gmail.com.ar>
2017-05-12 21:17:49 -0300

    [master] fixed bootstrap issues

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './components/App';
 
 global.jQuery = require('jquery');
 require('bootstrap-loader');
-require('font-awesome-webpack-sass');
 
 const json = require('./resume.json');
 
